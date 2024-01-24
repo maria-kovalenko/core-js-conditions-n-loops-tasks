2dbebba1f9ea48577c2609559827ec8e3a148faa
Maria <workmail358@gmail.com>
2024-01-24 22:54:21 +0300

    feat: add isContainNumber task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -185,8 +185,15 @@ function getIndexOf(/* str, letter */) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const arr = String(num);
+
+  for (let i = 0; i < arr.length; i += 1) {
+    if (+arr[i] === digit) {
+      return true;
+    }
+  }
+  return false;
 }
 
 /**
